5e800996d85d75a414815689ca20a56df2fe75c1
Davirshark <davirodrigo2524@gmail.com>
2023-10-05 14:39:22 -0300

    criando deploy

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y_hp#!zrb!qmud-9^p7lg2_)gaw+ge6b(dbj*22n5@*c=oopcf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
